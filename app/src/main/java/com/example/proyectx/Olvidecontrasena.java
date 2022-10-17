6a999cacf183790055807b70d2da632db3701c11
GunsOnly18 <ybo.acosta.flores@gmail.com>
2022-10-17 00:40:46 -0500

    trabajo pri

---
+++ app/src/main/java/com/example/proyectx/Olvidecontrasena.java
@@ -0,0 +1,14 @@
+package com.example.proyectx;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Olvidecontrasena extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_olvide_contrasena);
+    }
+}
